5eab1201e51587669169d074374281ed0e17930d
ca-rib <108152033+ca-rib@users.noreply.github.com>
2023-12-06 14:39:36 -0300

    Create main.java

---
+++ TPFINAL/main.java
@@ -0,0 +1,9 @@
+// Caroline Ribeiro e Leticia Vitoria
+
+public class main {
+    public static void main(String []args)
+    {
+        AlunoForm f = new AlunoForm("TRABALHO FINAL");
+        f.setVisible(true);
+    }
+}
