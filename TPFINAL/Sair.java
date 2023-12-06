b3f996890dcfdc1474b653fbd2780b77e222d3a5
ca-rib <108152033+ca-rib@users.noreply.github.com>
2023-12-06 14:26:26 -0300

    Create Sair.java

---
+++ TPFINAL/Sair.java
@@ -0,0 +1,18 @@
+// Caroline Ribeiro e Leticia Vitoria
+
+import java.awt.event.*;
+
+public class SairAction
+    extends WindowAdapter
+    implements ActionListener
+{
+    public void windowClosing(WindowEvent e)
+    {
+        System.exit(0);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+    }
+}
