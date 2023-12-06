a26c9ae44dd39cf651d5624eb8738d0a5142bde2
ca-rib <108152033+ca-rib@users.noreply.github.com>
2023-12-06 14:31:21 -0300

    Create Limpar.java

---
+++ TPFINAL/Limpar.java
@@ -0,0 +1,24 @@
+// Caroline Ribeiro e Leticia Vitoria
+
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.text.JTextComponent;
+
+public class LimparCamposAction implements ActionListener{
+
+    private List<JTextComponent> campos;
+
+    public LimparCamposAction(List<JTextComponent> arrayCampos) {
+        super();
+
+        campos = arrayCampos;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (JTextComponent jTextComponent : campos) {
+            jTextComponent.setText("");
+        }
+    }
+    
+}
